ca79a8dee12b22b8333d9386311d7a05ebd9898f
Gaston_Lions <gastonlions.pro@gmail.com>
2023-07-29 19:13:36 +0200

    verification nombre de building par ligne (erreurs valgrind gérées)

--- rush/rush01/brouillon/main.c
+++ rush/rush01/brouillon/main.c
@@ -7,21 +7,146 @@
 
 /*
 Mettre en place la fonction de backtracking
-1. Fonctionne uniquement par ligne
+1. Fonctionne uniquement par ligne -> OK
 2. Fonctionne avec les colonnes en plus des lignes
 3. Vérifie si le nombre de batiments visibles est ok
 */
 
+void	split_arg(char *src, char *dst1, int key)
+{
+	int	k;
+	int	size;
+
+	k = 0;
+	size = ft_strlen(src);
+	while(k < size / 4)
+	{
+		dst1[k] = src[key];
+		key++;
+		k++;
+	}
+	dst1[k] = '\0';
+	printf("Valeur final de l'arg : %s\n", dst1);
+}
+
+void	display_line(int *line, int size)
+{
+	int	i;
+
+	i = 0;
+	printf("Ma ligne possède : ");
+	while (i < size)
+	{
+		printf("%2d", line[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+int	verif_building_liner(int *line, int y, char *arg, int nb)
+{
+	int		i;
+	int 	size;
+	int		c;
+	int 	max;
+	char	*cond;
+	int		k;
+
+	max = 0;
+	c = 0;
+	k = 0;
+	size = ft_strlen(arg);
+	i = size / 4 - 1;
+	printf("Entrée dans verif_building_liner\n");
+	printf("Size = %d\n", size);
+	display_line(line, ft_strlen(arg) / 4);
+	while(i >= 0)
+	{
+		printf("Valeur de i = %d\n", i);
+		if (line[i] > max && line[i] != 0 && line[i] > nb)
+		{
+			c++;
+			max = line[i];
+		}
+		if (line[i] != 0)
+			k++;
+		i--;
+	}
+	k++;
+	printf("Case remplies : %d\n", k);
+	c++;
+	cond = (char *)malloc((size / 4 + 1) * sizeof(char));
+	if (!cond)
+		return (0);
+	split_arg(arg, cond, ft_strlen(arg) - ft_strlen(arg) / 4);
+	printf("cond = %s\n", cond);
+	printf("batiment(s) visible(s) -> %d\n", c);
+	printf("Je regarde -> %c\n", cond[y]);
+	if ((c - (cond[y] - '0') > 0 && max == 4)
+		|| (c - (cond[y] - '0') != 0 && k == ft_strlen(cond)))
+	{
+		printf("Sortie de verif_building_linel\n");
+		printf("Condition non remplie\n");
+		return (free(cond), (0));
+
+	}
+	printf("Sortie de verif_building_linel\n");
+	return (free(cond), (1));
+}
+
+int	verif_building_linel(int *line, int y, char *arg, int nb)
+{
+	int		i;
+	int 	size;
+	int		c;
+	int 	max;
+	char	*cond;
+
+	i = 0;
+	max = 0;
+	c = 0;
+	size = ft_strlen(arg);
+	printf("Entrée dans verif_building_linel\n");
+	printf("Size = %d\n", size);
+	display_line(line, ft_strlen(arg) / 4);
+	while(i < size / 4)
+	{
+		if (line[i] > max && line[i] != 0)
+		{
+			c++;
+			max = line[i];
+		}
+		i++;
+	}
+	if (nb > max)
+		c++;
+	cond = (char *)malloc((size / 4 + 1) * sizeof(char));
+	if (!cond)
+		return (0);
+	split_arg(arg, cond, ft_strlen(arg) - ((ft_strlen(arg) / 4) * 2));
+	printf("batiment(s) visible(s) -> %d\n", c);
+	printf("Je regarde -> %c\n", cond[y]);
+	if (c - (cond[y] - '0') > 0)
+	{
+		printf("Sortie de verif_building_linel\n");
+		printf("Condition non remplie\n");
+		return (free(cond), (0));
+
+	}
+	printf("Sortie de verif_building_linel\n");
+	return (free(cond), (1));
+}
+
 int	verif_line(int **grille, char *arg, int pos, int nb)
 {
-	int	y;
-	int	i;
+	int		y;
+	int		i;
 
 	y = pos / (ft_strlen(arg) / 4);
 	i = 0;
 	while (i < ft_strlen(arg) / 4)
 	{
-		if(grille[y][i] == nb){
+		if (grille[y][i] == nb){
 			return (0);
 		}
 		i++;
@@ -29,7 +154,7 @@ int	verif_line(int **grille, char *arg, int pos, int nb)
 	return (1);
 }
 
-int	get_size_building(int **grille, char * arg, int pos)
+/*int	get_size_building(int **grille, char * arg, int pos)
 {
 	printf("- Fonction gez_size_building -> ");
 	int	i;
@@ -45,40 +170,44 @@ int	get_size_building(int **grille, char * arg, int pos)
 		i++;
 	}
 	return (0);
-}
+}*/
 
 int ft_backtracking(int **grille, char *arg, int pos)
 {
-	printf("--- Entrée n°%d dans la fonction backtracking ---\n", pos + 1);
-	int	y;
-	int	x;
-	
-	printf("Arguments donnés: ARG = %s | POS = %d\n", arg, pos);
-	printf("Etat de la grille au départ : \n");
+	int		y;
+	int		x;
+	int		i;
+
+	printf("Travail sur la position n°%d de la grille\n", pos);
 	display_grille(grille, ft_strlen(arg) / 4);
-	if (pos == ft_strlen(arg))
-	{
-		printf("Grille pleine\n");
-		display_grille(grille, ft_strlen(arg) / 4);
+	printf("%d | %d\n", pos, (ft_strlen(arg) / 4) * (ft_strlen(arg) / 4));
+	// if (pos == (ft_strlen(arg) / 4) * 2 + 1)
+	if (pos == (ft_strlen(arg) / 4) * (ft_strlen(arg) / 4))
+	{        
+		printf("GRILLE PLEINE\n");		
 		return (1);
 	}
 	y = pos / (ft_strlen(arg) / 4);
 	x = pos % (ft_strlen(arg) / 4);
-	printf("y = %d | x = %d\n", y, x);
-	if (grille[y][x] != 0)
-	{
-		printf("Case déjà remplie\n");
+    if (grille[y][x] != 0)
 		return (ft_backtracking(grille, arg, pos + 1));
-	}
-	grille[y][x] = get_size_building(grille, arg, pos);
-	printf("Nouvelle valeur de la case : %d\n", grille[y][x]);
-	if (grille[y][x] == 0)
-	{
-		printf("Pas de possibilité\n");
-		return (0);
-	}
-	printf("On continue le boulot !\n");
-	return (ft_backtracking(grille, arg, pos + 1));
+	i = 1;
+	while (i <= ft_strlen(arg) / 4)
+    {
+		printf("Insertion de %d dans grille[%d][%d]\n", i, y, x);
+		display_grille(grille, ft_strlen(arg) / 4); 
+        if (verif_line(grille, arg, pos, i) 
+			&& verif_building_linel(grille[y], y, arg, i)
+			&& verif_building_liner(grille[y], y, arg, i))
+        {
+            grille[y][x] = i;
+            if (ft_backtracking(grille, arg, pos + 1))
+	                return (1);
+        }
+		grille[y][x] = 0;
+		i++;
+    }
+    return (0);
 }
 
 int	error_msg()
